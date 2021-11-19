2ec230e1e3ebb59c0c8cb137f76d4d02a65b80d5
Nadhif Rafifaiz K <nadhifrafifaiz@gmail.com>
2021-11-19 14:10:06 +0700

    [Meeting 2] Tidy up

--- math-game/src/scenes/MathFighterScene.js
+++ math-game/src/scenes/MathFighterScene.js
@@ -79,13 +79,14 @@ export default class MathFighterScene extends Phaser.Scene {
 
         this.createAnimation()
 
+        this.createButtons()
+
         let start_button = this.add.image(this.gameHalfWidth, this.gameHalfHeight + 181, 'start-btn').setInteractive()
         start_button.on('pointerdown', () => {
             this.gameStart()
             start_button.destroy()
         }, this)
 
-        this.createButtons()
     }
     update() {
 
@@ -147,55 +148,28 @@ export default class MathFighterScene extends Phaser.Scene {
         this.player.anims.play('player-standby', true)
         this.enemy.anims.play('enemy-standby', true)
 
-        this.resultText = this.add.text(this.gameHalfWidth, 400, '0', { fontSize: '32px', fill: "#000" })
-        this.questionText = this.add.text(this.gameHalfWidth, 200, '0', { fontSize: '32px', fill: "#000" })
+        this.resultText = this.add.text(this.gameHalfWidth, 200, '0', { fontSize: '32px', fill: "#000" })
+        this.questionText = this.add.text(this.gameHalfWidth, 100, '0', { fontSize: '32px', fill: "#000" })
     }
     createButtons() {
         const startPositionY = this.scale.height - 246
         const widthDifference = 131
         const heightDifference = 71.25
-        this.button2 = this.add.image(this.gameHalfWidth,
-            startPositionY, 'numbers', 0)
-            .setInteractive().setData('value', 2)
-        this.button5 = this.add.image(this.gameHalfWidth,
-            this.button2.y + heightDifference, 'numbers', 4)
-            .setInteractive().setData('value', 5)
-        this.button8 = this.add.image(this.gameHalfWidth,
-            this.button5.y + heightDifference, 'numbers', 7)
-            .setInteractive().setData('value', 8)
-        this.button0 = this.add.image(this.gameHalfWidth,
-            this.button8.y + heightDifference, 'numbers', 10)
-            .setInteractive().setData('value', 0)
-        this.button1 = this.add.image(this.button2.x -
-            widthDifference, startPositionY, 'numbers', 0)
-            .setInteractive().setData('value', 1)
-        this.button4 = this.add.image(this.button5.x -
-            widthDifference, this.button1.y +
-        heightDifference, 'numbers', 3)
-            .setInteractive().setData('value', 4)
-        this.button7 = this.add.image(this.button8.x -
-            widthDifference, this.button4.y +
-        heightDifference, 'numbers', 6)
-            .setInteractive().setData('value', 7)
-        this.buttonDel = this.add.image(this.button0.x -
-            widthDifference, this.button7.y +
-        heightDifference, 'numbers', 9)
-            .setInteractive().setData('value', 'del')
-        this.button3 = this.add.image(this.button2.x +
-            widthDifference, startPositionY, 'numbers', 2)
-            .setInteractive().setData('value', 3)
-        this.button6 = this.add.image(this.button5.x +
-            widthDifference, this.button3.y +
-        heightDifference, 'numbers', 5)
-            .setInteractive().setData('value', 6)
-        this.button9 = this.add.image(this.button8.x +
-            widthDifference, this.button6.y +
-        heightDifference, 'numbers', 8)
-            .setInteractive().setData('value', 9)
-        this.buttonOk = this.add.image(this.button0.x +
-            widthDifference, this.button9.y +
-        heightDifference, 'numbers', 11)
-            .setInteractive().setData('value', 'ok')
+        // namaButton=this.add.image(koordinatX, koordinatY)
+        this.button2 = this.add.image(this.gameHalfWidth, startPositionY, 'numbers', 1).setInteractive().setData('value', 2)
+        this.button5 = this.add.image(this.gameHalfWidth, this.button2.y + heightDifference, 'numbers', 4).setInteractive().setData('value', 5)
+        this.button8 = this.add.image(this.gameHalfWidth, this.button5.y + heightDifference, 'numbers', 7).setInteractive().setData('value', 8)
+        this.button0 = this.add.image(this.gameHalfWidth, this.button8.y + heightDifference, 'numbers', 10).setInteractive().setData('value', 0)
+
+        this.button1 = this.add.image(this.button2.x - widthDifference, startPositionY, 'numbers', 0).setInteractive().setData('value', 1)
+        this.button4 = this.add.image(this.button5.x - widthDifference, this.button1.y + heightDifference, 'numbers', 3).setInteractive().setData('value', 4)
+        this.button7 = this.add.image(this.button8.x - widthDifference, this.button4.y + heightDifference, 'numbers', 6).setInteractive().setData('value', 7)
+        this.buttonDel = this.add.image(this.button0.x - widthDifference, this.button7.y + heightDifference, 'numbers', 9).setInteractive().setData('value', 'del')
+
+        this.button3 = this.add.image(this.button2.x + widthDifference, startPositionY, 'numbers', 2).setInteractive().setData('value', 3)
+        this.button6 = this.add.image(this.button5.x + widthDifference, this.button3.y + heightDifference, 'numbers', 5).setInteractive().setData('value', 6)
+        this.button9 = this.add.image(this.button8.x + widthDifference, this.button6.y + heightDifference, 'numbers', 8).setInteractive().setData('value', 9)
+        this.buttonOk = this.add.image(this.button0.x + widthDifference, this.button9.y + heightDifference, 'numbers', 11).setInteractive().setData('value', 'ok')
     }
 
 }
